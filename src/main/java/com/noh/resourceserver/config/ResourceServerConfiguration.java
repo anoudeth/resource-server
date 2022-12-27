23f857c731e9410166dcdac618c15e3c0173b8b3
nohder <pisces2531@gmail.com>
2022-12-27 08:29:04 +0700

    done

--- src/main/java/com/noh/resourceserver/config/ResourceServerConfiguration.java
+++ src/main/java/com/noh/resourceserver/config/ResourceServerConfiguration.java
@@ -26,7 +26,16 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		System.out.println("hello");
-		http.authorizeRequests().anyRequest().permitAll().and().cors().disable().csrf().disable().httpBasic().disable()
+		http.authorizeRequests()
+				.anyRequest()
+				.permitAll()
+				.and()
+				.cors()
+				.disable()
+				.csrf()
+				.disable()
+				.httpBasic()
+				.disable()
 				.exceptionHandling()
 				.authenticationEntryPoint(
 						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
